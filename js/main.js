a8b3e78fa0a03880b34fd9ea6d3bdb65509200b1
ne0chem <brenkoangelina@gmail.com>
2025-10-20 19:12:35 +0300

    секция последние проекты

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 new WOW().init();
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+
+  pagination: {
+    el: ".project-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
